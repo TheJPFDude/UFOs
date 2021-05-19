f65bfbe0c68cf4d7e2cad2d2789f57dcda14edfd
bchoi02 <bogeunchoi@berkeley.edu>
2021-05-19 16:05:44 -0500

    added for loop in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,19 @@ const tableData = data;
 
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // Clear out existing data
+    tbody.html("");
+
+    // Loop through each object in data
+    // and append a row/cells for each value in row
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+          let cell = row.append("td");
+          cell.text(val);
+          }
+        );
+    });
+  }
